84eeb6498d340922e587433a6198bf0c071fb16f
Stan Makov <makovstpm@gmail.com>
2024-08-26 21:28:41 +0200

    Flawed database updating

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 import openpyxl
 
-# Objective 1: Скопировать из source в output
+# Objective 1: Скопировать из source в conversion
 
 # Get source file
 source = openpyxl.load_workbook('source.xlsx')
 source_sheet = source.active
 
 # Get conversion file
-conversion = openpyxl.load_workbook('output.xlsx')
+conversion = openpyxl.load_workbook('conversion.xlsx')
 conversion_sheet = conversion.worksheets[0]
 
 source_range = source_sheet['E5:G8']
@@ -17,18 +17,19 @@ for i, row in enumerate(source_range):
         if not isinstance(cell, openpyxl.cell.cell.MergedCell):
             conversion_sheet.cell(row=5 + i, column=5 + j, value=cell.value)
 
-conversion.save('output.xlsx')
+conversion.save('conversion.xlsx')
 
 print("Objective 1 ✅")
 
 # Objective 2
-conversion_transposed = conversion.worksheets[1]
+conversionValue = openpyxl.load_workbook('conversion.xlsx', data_only=True)
+conversion_transposed = conversionValue.worksheets[1]
 
 database = openpyxl.load_workbook('database.xlsx')
 database_sheet = database.active
 
 transposed_range = conversion_transposed['C6:F8']
-
+print(conversion_sheet['D7'].value)
 empty_row = database_sheet.max_row + 1
 
 for i, row in enumerate(transposed_range):

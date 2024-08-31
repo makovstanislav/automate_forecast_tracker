e36d103a9aa6c4f52056b51409c71cce481087eb
Stan Makov <makovstpm@gmail.com>
2024-08-31 17:45:13 +0200

    Change paths to excel files

--- script.py
+++ script.py
@@ -4,41 +4,41 @@ import xlwings as xw
 # Objective 1: Скопировать из source в conversion
 
 # Загрузка исходного Excel-файла source.xlsx
-source = openpyxl.load_workbook('source.xlsx')
-source_sheet = source.active
+source = openpyxl.load_workbook('../real_data/source.xlsx')
+source_sheet_NA = source.worksheets[0]
 
 # Загрузка файла conversion.xlsx
-conversion = openpyxl.load_workbook('conversion.xlsx')
-conversion_sheet = conversion.worksheets[0]
+conversion = openpyxl.load_workbook('../real_data/conversion.xlsx')
+conversion_sheet_paste = conversion.worksheets[0]
 
-# Копирование диапазона E5:G8 из source.xlsx в первый лист conversion.xlsx
-source_range = source_sheet['E5:G8']
+# Копирование диапазона E5:CK59 из source.xlsx NA в первый лист conversion.xlsx
+source_range = source_sheet_NA['E5:CK59']
 for i, row in enumerate(source_range):
     for j, cell in enumerate(row):
         if not isinstance(cell, openpyxl.cell.cell.MergedCell):
-            conversion_sheet.cell(row=5 + i, column=5 + j, value=cell.value)
+            conversion_sheet_paste.cell(row=5 + i, column=5 + j, value=cell.value)
 
 # Сохранение изменений в файле conversion.xlsx
-conversion.save('conversion.xlsx')
+conversion.save('../real_data/conversion.xlsx')
 
 print("Objective 1 ✅")
 
 # Objective 2: Использование xlwings для расчета и получения значений
 
 # Открываем файл с помощью xlwings
-wbxl = xw.Book('conversion.xlsx')
+wbxl = xw.Book('../real_data/conversion.xlsx')
 
 # Выбираем второй лист
-sheet2 = wbxl.sheets['Лист2']
+transposed = wbxl.sheets['Transposed']
 
-# Извлечение значений из диапазона C6:F8 после автоматического пересчета формул
-calculated_values = sheet2.range('C6:F8').value
+# Извлечение значений из диапазона A4:Y122 после автоматического пересчета формул
+calculated_values = transposed.range('A4:Y122').value
 
 # Закрытие файла, сохранение не нужно, так как значения уже извлечены
 wbxl.close()
 
 # Загрузка файла database.xlsx
-database = openpyxl.load_workbook('database.xlsx')
+database = openpyxl.load_workbook('../real_data/database.xlsx')
 database_sheet = database.active
 
 # Поиск первой пустой строки в database.xlsx
@@ -50,6 +50,6 @@ for i, row in enumerate(calculated_values):
         database_sheet.cell(row=empty_row + i, column=3 + j, value=value)
 
 # Сохранение изменений в файле database.xlsx
-database.save('database.xlsx')
+database.save('../real_data/database.xlsx')
 
 print("Objective 2 ✅")
